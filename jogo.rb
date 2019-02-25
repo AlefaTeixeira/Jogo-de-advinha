932b23a127e91acd893c3bcb6dad2b55f757b396
Alef Aparecido De Jesus Teixeira <1001627@PPC01INFLL00312.pucminas.net>
2019-02-25 19:55:55 -0300

    #n: mensagem.

--- jogo.rb
+++ jogo.rb
@@ -1,17 +1,61 @@
-puts "nome"
-nome = gets
-puts "chute"
-chute = gets
+    puts "Bem-vindo ao jogo da adivinhacao"
+    puts "Qual e o seu nome?"
+    nome = gets
+    puts "\n\n\n\n"
+    puts "Comecaremos o jogo para voce, " + nome
+    puts "Escolhendo um numero secreto entre 0 e 200..."
+    numero_secreto = 121
+    puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
+    puts "\n\n\n"
+    puts "Tentativa 1"
+    puts "Entre com o numero"
+    chute = gets
+    puts "Sera que acertou? Voce chutou " + chute
+    acertou = numero_secreto == chute.to_i
 
-segredo = 100
-puts nome
-puts segredo == chute.to_i
+if acertou
+    puts "Acertou!"
+    maior = numero_secreto > chute.to_i
 
-if
-unless segredo == 120
-puts "maior"
-else if
-unless segredo == 130
-puts"maior"
+if maior
+    puts "O numero secreto e maior!"
+else
+    puts "O numero secreto e menor!"
 end
+end
+    puts "\n\n\n"
+    puts "Tentativa 2"
+    puts "Entre com o numero"
+    chute = gets
+    puts "Sera que acertou? Voce chutou " + chute
+    acertou = numero_secreto == chute.to_i
+if acertou
+    puts "Acertou!"
+else
+    maior = numero_secreto > chute.to_i
 
+if maior
+    puts "O numero secreto e maior!"
+else
+    puts "O numero secreto e menor!"
+end
+end
+    puts "\n\n\n"
+    puts "Tentativa 3"
+    puts "Entre com o numero"
+    chute = gets
+    puts "Sera que acertou? Voce chutou " + chute
+    acertou = numero_secreto == chute.to_i
+
+if acertou
+    puts "Acertou!" 
+else
+    maior = numero_secreto > chute.to_i
+
+if maior
+
+    puts "O numero secreto e maior!"
+else  
+    puts "O numero secreto e menor!"
+end
+end
